e514913eb4b10ce0365420f55a2d8c37aa4240ea
Masihafazlpour <121658500+Masihafazlpour@users.noreply.github.com>
2026-08-04 19:21:33 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://ezxyxsxjlzaxkpmaudwi.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_IIPfIbikyTq0e9LwAd6I9A_hANvHH2L',
   FORM_SLUG: 'student-registration',
   SCHOOL_NAME: 'دبیرستان نمونه دولتی شهید شیرآقایی بهشهر',
   APP_NAME: 'سامانه ثبت اطلاعات دانش‌آموز',
