e5ed8056014cf0b060b61eae4ec81b3ea8df8a7d
Masihafazlpour <121658500+Masihafazlpour@users.noreply.github.com>
2026-08-04 18:53:48 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = Object.freeze({
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: '',
+  FORM_SLUG: 'student-registration',
+  SCHOOL_NAME: 'دبیرستان نمونه دولتی شهید شیرآقایی بهشهر',
+  APP_NAME: 'سامانه ثبت اطلاعات دانش‌آموز',
+});
